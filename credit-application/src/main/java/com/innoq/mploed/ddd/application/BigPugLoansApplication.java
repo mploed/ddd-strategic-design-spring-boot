78f8e5a6a417f62476e64eef86098ba26c3c6595
mploed <michael.ploed@gmail.com>
2016-09-08 14:12:53 +0200

    Changes to central web application credit-application

---
+++ credit-application/src/main/java/com/innoq/mploed/ddd/application/BigPugLoansApplication.java
@@ -0,0 +1,11 @@
+package com.innoq.mploed.ddd.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigPugLoansApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BigPugLoansApplication.class);
+    }
+}
---
+++ credit-application/src/main/java/com/innoq/mploed/ddd/application/domain/CreditApplicationForm.java
@@ -0,0 +1,110 @@
+package com.innoq.mploed.ddd.application.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class CreditApplicationForm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    private Long id;
+
+    private int term;
+
+    private BigDecimal amount;
+
+    private String purpose;
+
+    private BigDecimal percentage;
+
+    private BigDecimal monthlyPayment;
+
+    @Embedded
+    private SelfDisclosure selfDisclosure;
+
+    private Long customerId;
+
+
+    public CreditApplicationForm() {
+        selfDisclosure = new SelfDisclosure();
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getTerm() {
+        return term;
+    }
+
+    public void setTerm(int term) {
+        this.term = term;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getPurpose() {
+        return purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.purpose = purpose;
+    }
+
+    public BigDecimal getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(BigDecimal percentage) {
+        this.percentage = percentage;
+    }
+
+    public BigDecimal getMonthlyPayment() {
+        return monthlyPayment;
+    }
+
+    public void setMonthlyPayment(BigDecimal monthlyPayment) {
+        this.monthlyPayment = monthlyPayment;
+    }
+
+    public SelfDisclosure getSelfDisclosure() {
+        return selfDisclosure;
+    }
+
+    public void setSelfDisclosure(SelfDisclosure selfDisclosure) {
+        this.selfDisclosure = selfDisclosure;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    @Override
+    public String toString() {
+        return "CreditApplicationForm{" +
+                "id=" + id +
+                ", term=" + term +
+                ", amount=" + amount +
+                ", purpose='" + purpose + '\'' +
+                ", percentage=" + percentage +
+                ", monthlyPayment=" + monthlyPayment +
+                ", selfDisclosure=" + selfDisclosure +
+                ", customerId=" + customerId +
+                '}';
+    }
+}
